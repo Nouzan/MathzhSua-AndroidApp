350f2bf65141c6b7ead57c434891c03aeb085e48
Nouzan <clobraindie@outlook.com>
2017-11-25 01:23:19 +0800

    add Tables

---
+++ app/src/main/java/cn/edu/sysu/mathzh/mathzhsua/database/Tables.kt
@@ -0,0 +1,15 @@
+package cn.edu.sysu.mathzh.mathzhsua.database
+
+/**
+ * Created by nouzan on 2017/11/24.
+ */
+
+object SuaTable {
+    val ID = "_id"
+    val USER_ID = "userId"
+    val GROUP_ID = "groupId"
+    val TITLE = "title"
+    val TEAM = "team"
+    val DATE = "date"
+    val SUAHOURS = "suahours"
+}
--- app/src/main/java/cn/edu/sysu/mathzh/mathzhsua/ui/App.kt
+++ app/src/main/java/cn/edu/sysu/mathzh/mathzhsua/ui/App.kt
@@ -1,14 +1,15 @@
 package cn.edu.sysu.mathzh.mathzhsua.ui
 
 import android.app.Application
+import kotlin.properties.Delegates
 
 /**
  * Created by nouzan on 2017/11/23.
  */
 class App : Application() {
     companion object {
-        private var instance: Application? = null
-        fun instance() = instance!!
+        private var instance: App by Delegates.notNull()
+        //fun instance() = instance!!
     }
 
     override fun onCreate() {
